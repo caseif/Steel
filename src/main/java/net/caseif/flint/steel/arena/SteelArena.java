3b8a897c352d671972da8b45ce463b6dade272f9
Max Roncace <me@caseif.net>
2022-03-18 09:03:12 -0400

    Catch all Throwables when marking blocks for rollback

--- src/main/java/net/caseif/flint/steel/arena/SteelArena.java
+++ src/main/java/net/caseif/flint/steel/arena/SteelArena.java
@@ -90,7 +90,7 @@ public class SteelArena extends CommonArena {
         try {
             Location loc = LocationHelper.convertLocation(location);
             getRollbackAgent().logBlockChange(loc.getBlock());
-        } catch (IOException | SQLException ex) {
+        } catch (Throwable ex) {
             throw new RollbackException(ex);
         }
     }
